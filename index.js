1e02f2367b48150c122be78b1efcea5701c5bc2c
Rambeboy <kingindoslayer@gmail.com>
2024-11-09 22:24:13 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,152 @@
+import { privateKey } from "./accounts/accounts.js";
+import { Config } from "./config/config.js";
+import Core from "./src/core/core.js";
+import sqlite from "./src/core/db/sqlite.js";
+import { Helper } from "./src/utils/helper.js";
+import logger from "./src/utils/logger.js";
+import twist from "./src/utils/twist.js";
+
+async function operation(acc) {
+  await sqlite.connectToDatabase();
+  await sqlite.createTable();
+  const core = new Core(acc);
+  try {
+    await core.connectWallet();
+    await core.getBalance();
+
+    if (core.balance.ETH < 0.0015)
+      throw Error("Minimum Eth Balance Is 0.0015 ETH");
+    if (Config.USEWRAPUNWRAP ?? true) {
+      const currentCount =
+        Number(Config.WRAPUNWRAPCOUNT) -
+        Number((await sqlite.getTodayTxLog(core.address, "tx")).length);
+      const txCount = currentCount > 0 ? currentCount : 0;
+      for (const count of Array(txCount)) {
+        if (core.balance.ETH < 0.0015)
+          throw Error(
+            "Balance is less than 0.0015 ETH, please fill up your balance"
+          );
+        try {
+          await core.deposit();
+          await core.withdraw();
+          await sqlite.insertData(core.address, new Date().toISOString(), "tx");
+        } catch (error) {
+          await Helper.delay(
+            3000,
+            acc,
+            `Error during deposit/withdraw operation: ${error.message}`,
+            core
+          );
+        }
+        const delay = Helper.random(10000, 60000 * 2);
+        await Helper.delay(
+          delay,
+          acc,
+          `Delaying for ${Helper.msToTime(delay)} Before Executing Next TX`,
+          core
+        );
+      }
+    }
+    if (
+      (Config.USERAWTXDATA ?? false) &&
+      Config.RAWTX != undefined &&
+      Config.RAWTX !=
+        {
+          CONTRACTTOINTERACT: "CONTRACTADDRESSTOINTERACT",
+          RAWDATA: "RAWDATA",
+        }
+    ) {
+      const currentCount =
+        Number(Config.RAWTXCOUNT) -
+        Number((await sqlite.getTodayTxLog(core.address, "raw")).length);
+      const txCount = currentCount > 0 ? currentCount : 0;
+      for (const tx of Array(txCount)) {
+        await core.rawTx();
+        await sqlite.insertData(core.address, new Date().toISOString(), "raw");
+      }
+    }
+
+    if (Config.USESELFTRANSFER ?? false) {
+      const currentCount =
+        Number(Config.SELFTRANSFERCOUNT) -
+        Number((await sqlite.getTodayTxLog(core.address, "self")).length);
+      const txCount = currentCount > 0 ? currentCount : 0;
+      for (const tx of Array(txCount)) {
+        await core.transfer();
+        await sqlite.insertData(core.address, new Date().toISOString(), "self");
+      }
+    }
+
+    const delay = 60000 * 60 * 24;
+    await Helper.delay(
+      delay,
+      acc,
+      `Account ${
+        privateKey.indexOf(acc) + 1
+      } Processing Done, Delaying for ${Helper.msToTime(delay)}`,
+      core
+    );
+    await operation(acc);
+  } catch (error) {
+    if (error.message) {
+      await Helper.delay(
+        10000,
+        acc,
+        `Error : ${error.message}, Retry again after 10 Second`,
+        core
+      );
+    } else {
+      await Helper.delay(
+        10000,
+        acc,
+        `Error :${JSON.stringify(error)}, Retry again after 10 Second`,
+        core
+      );
+    }
+
+    await operation(acc);
+  }
+}
+
+async function startBot() {
+  return new Promise(async (resolve, reject) => {
+    try {
+      logger.info(`BOT STARTED`);
+      if (privateKey.length == 0)
+        throw Error("Please input your account first on accounts.js file");
+      const promiseList = [];
+
+      for (const acc of privateKey) {
+        promiseList.push(operation(acc));
+      }
+
+      await Promise.all(promiseList);
+      resolve();
+    } catch (error) {
+      logger.info(`BOT STOPPED`);
+      logger.error(JSON.stringify(error));
+      reject(error);
+    }
+  });
+}
+
+(async () => {
+  try {
+    logger.clear();
+    logger.info("");
+    logger.info("Application Started");
+    console.log("EVM TX DEPLOYER BOT");
+    console.log();
+    console.log("Author : Rambeboy");
+    console.log("Github : https://github.com/Rambeboy");
+    console.log("Dont forget to run git pull to keep up to date");
+    console.log();
+    console.log();
+    await startBot();
+  } catch (error) {
+    twist.clear();
+    twist.clearInfo();
+    console.log("Error During executing bot", error);
+    await startBot();
+  }
+})();
---
+++ src/utils/twist.js
@@ -0,0 +1,74 @@
+import { Twisters } from "twisters";
+import logger from "./logger.js";
+import Core from "../core/core.js";
+import { privateKey } from "../../accounts/accounts.js";
+import { RPC } from "../core/network/rpc.js";
+import { Config } from "../../config/config.js";
+import sqlite from "../core/db/sqlite.js";
+
+class Twist {
+  constructor() {
+    /** @type  {Twisters}*/
+    this.twisters = new Twisters();
+  }
+
+  /**
+   * @param {string} acc
+   * @param {Core} core
+   * @param {string} msg
+   * @param {string} delay
+   */
+  async log(msg = "", acc = "", core = new Core(), delay) {
+    const accIdx = privateKey.indexOf(acc);
+    if (delay == undefined) {
+      logger.info(`Account ${accIdx + 1} - ${msg}`);
+      delay = "-";
+    }
+
+    const address = core.address ?? "-";
+    const balance = core.balance ?? {};
+    const eth = balance.ETH ?? "-";
+    const weth = balance.WETH ?? "-";
+    const wethSymbol = core.tokenSymbol ?? "-";
+
+    const tx = (await sqlite.getTodayTxLog(address, "tx")).length;
+    const raw = (await sqlite.getTodayTxLog(address, "raw")).length;
+    const self = (await sqlite.getTodayTxLog(address, "self")).length;
+
+    this.twisters.put(acc, {
+      text: `
+================== Account ${accIdx + 1} =================
+Address         : ${address}
+Balance         : ${eth} ${RPC.SYMBOL}
+                  ${weth} ${wethSymbol}
+W/U Count       : ${tx} of ${Config.WRAPUNWRAPCOUNT ?? "?"}
+RAWTX Count     : ${raw} of ${Config.RAWTXCOUNT ?? "?"}
+Transfer Count  : ${self} of ${Config.SELFTRANSFERCOUNT ?? "?"}
+               
+Status : ${msg}
+Delay : ${delay}
+==============================================`,
+    });
+  }
+  /**
+   * @param {string} msg
+   */
+  info(msg = "") {
+    this.twisters.put(2, {
+      text: `
+==============================================
+Info : ${msg}
+==============================================`,
+    });
+    return;
+  }
+
+  clearInfo() {
+    this.twisters.remove(2);
+  }
+
+  clear(acc) {
+    this.twisters.remove(acc);
+  }
+}
+export default new Twist();
